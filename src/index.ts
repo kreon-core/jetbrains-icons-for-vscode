59fb7f40f29df4d5aad1a6aefc8dc5ed22e7612b
Crix Dang <crix.dang@gmail.com>
2025-11-02 00:36:26 +0700

    fix: refactor some parts of codebase (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import * as vscode from "vscode";
-import { constants } from "./constants";
-
-export function activate(context: vscode.ExtensionContext): void {
-  if (context.globalState.get("installed") !== true) {
-    vscode.window.showInformationMessage(`${constants.extension.displayName} is installed!`);
-    context.globalState.update("installed", true);
-  }
-}
-
+export function activate(): void {}
 export function deactivate() {}
