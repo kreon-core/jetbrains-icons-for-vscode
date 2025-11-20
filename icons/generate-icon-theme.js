663af5564d0e8123ac08b18d8a922c2f30b3b1ed
Crix Dang <crix.dang@gmail.com>
2025-11-20 22:35:52 +0700

    feat(refactor): reorganize icons

--- icons/generate-icon-theme.js
+++ icons/generate-icon-theme.js
@@ -3,12 +3,16 @@ const { writeFileSync, readFileSync } = require("fs");
 const folderIcons = JSON.parse(readFileSync("./icons/folder-icons.json"));
 const fileIcons = JSON.parse(readFileSync("./icons/file-icons.json"));
 
-const lightMode = JSON.parse(readFileSync("./icons/icon-theme-light-mode.json"));
-const darkMode = JSON.parse(readFileSync("./icons/icon-theme-dark-mode.json"));
+const fileLightMode = JSON.parse(readFileSync("./icons/file-icon-theme-light-mode.json"));
+const folderLightMode = JSON.parse(readFileSync("./icons/folder-icon-theme-light-mode.json"));
+const fileDarkMode = JSON.parse(readFileSync("./icons/file-icon-theme-dark-mode.json"));
+const folderDarkMode = JSON.parse(readFileSync("./icons/folder-icon-theme-dark-mode.json"));
+
 const final = {
   iconDefinitions: { ...folderIcons, ...fileIcons },
-  light: { ...lightMode },
-  ...darkMode
+  light: { ...fileLightMode, ...folderLightMode },
+  ...fileDarkMode,
+  ...folderDarkMode
 };
 
 writeFileSync("./icons/jetbrains-icon-theme.json", JSON.stringify(final, null, 2));
